9df8f77ac70cf6db9c9ee80d084eddf80ab3bba5
Dennis White <dwhite5@ncsu.edu>
2022-10-24 21:24:43 -0400

    basic structure

---
+++ RivenMagaementSystems/src/main/java/Costs/CostOfGoodsSold.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Costs;
+
+/**
+ *
+ * @author dpw82
+ */
+public class CostOfGoodsSold implements CommonCosts{
+
+    @Override
+    public double totalCosts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private double SUVcost;
+    private double SEDANcost;
+    private double SPORTScost;
+    private String CarName;
+
+    /**
+     * @return the SUVcost
+     */
+    public double getSUVcost() {
+        return SUVcost;
+    }
+
+    /**
+     * @param SUVcost the SUVcost to set
+     */
+    public void setSUVcost(double SUVcost) {
+        this.SUVcost = SUVcost;
+    }
+
+    /**
+     * @return the SEDANcost
+     */
+    public double getSEDANcost() {
+        return SEDANcost;
+    }
+
+    /**
+     * @param SEDANcost the SEDANcost to set
+     */
+    public void setSEDANcost(double SEDANcost) {
+        this.SEDANcost = SEDANcost;
+    }
+
+    /**
+     * @return the SPORTScost
+     */
+    public double getSPORTScost() {
+        return SPORTScost;
+    }
+
+    /**
+     * @param SPORTScost the SPORTScost to set
+     */
+    public void setSPORTScost(double SPORTScost) {
+        this.SPORTScost = SPORTScost;
+    }
+
+    /**
+     * @return the CarName
+     */
+    public String getCarName() {
+        return CarName;
+    }
+
+    /**
+     * @param CarName the CarName to set
+     */
+    public void setCarName(String CarName) {
+        this.CarName = CarName;
+    }
+    
+    
+    
+}
---
+++ RivenMagaementSystems/src/main/java/Revenues/SedanSales.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Revenues;
+
+/**
+ *
+ * @author dpw82
+ */
+public class SedanSales implements CommonSalesData {
+
+    @Override
+    public double totalSales() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    ////
+}
